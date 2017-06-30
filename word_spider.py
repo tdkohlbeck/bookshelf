1957495573b42eddcc085f7de544abffea921bd3
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-06-30 01:11:40 -0400

    bag of words for url and its links (and its links, etc.)

--- word_spider.py
+++ word_spider.py
@@ -1,15 +1,37 @@
+from collections import defaultdict
 from bs4 import BeautifulSoup
 import pprint as p
-import re, urllib.request
+import operator, re, urllib.request
 
-class BookmarkCorpus:
-    def __init__(self, url, crawl_depth=1):
+class Bookmark:
+    def __init__(self, url):
         self.url = url
-        self.crawl_depth = crawl_depth
 
-        self.urls = []
+        self.crawl_depth = 0
+        try:
+            self.html = urllib.request.urlopen(self.url)
+            self.soup = BeautifulSoup(self.html, 'html.parser')
+            for elem in self.soup.find_all(['script', 'style']):
+                elem.extract()
+        except:
+            #print('cannot GET ' + self.url)
+            self.html = None
+            self.soup = None
 
+    def get_links(self, url=None):
+        url = url or self.url
+        if not self.soup:
+            return []
+        return [
+            link.get('href') for link in self.soup.find_all('a')
+        ]
 
+    def get_stopwords(self, location='stopwords.txt'):
+        with open(location) as f:
+            return set([
+                line[:-1] # except newline
+                for line in f.readlines()
+            ])
 
     def print_seed_url(self):
         print(self.url)
@@ -17,23 +39,14 @@ class BookmarkCorpus:
     def print_crawl_depth(self):
         print(self.crawl_depth)
 
-    def get_words(self):
-        html = urllib.request.urlopen(self.url).read()
-        soup = BeautifulSoup(html, 'html.parser')
-        for elem in soup.find_all(['script', 'style']):
-            elem.extract()
-        self.urls.append(
-            [ link['href'] for link in soup.find_all('a') ]
-        )
-
-        with open('stopwords.txt') as sw:
-            stopwords = []
-            for line in sw.readlines():
-                stopwords.append(line[:-1])
-        stopwords = set(stopwords)
+    def get_words(self, url=None):
+        if not self.soup:
+            return []
+        stopwords = self.get_stopwords()
+        #print(stopwords)
 
         words = [
-            word for word in soup.get_text().lower().split()
+            word for word in self.soup.get_text().lower().split()
             if word not in stopwords
         ]
         words = [
@@ -41,12 +54,31 @@ class BookmarkCorpus:
             for word in words
         ]
         words = list(filter(None, words))
-        p.pprint(words)
+        #p.pprint(words)
 
+        return words
 
-bm_corp = BookmarkCorpus('http://google.com', 3)
-bm_corp.get_words()
+    def get_bow(self, words):
+        freq = defaultdict(int)
+        for word in words:
+            freq[word] += 1
+            print(str(freq[word]) + " " + word)
+        p.pprint(sorted(freq.items(), key=operator.itemgetter(1)))
 
+bm = Bookmark('https://www.crummy.com/software/BeautifulSoup/bs4/doc/#')
+words = bm.get_words()
+links = bm.get_links()
+print(len(links))
+for link in links:
+    bm = Bookmark(link)
+    links.extend(bm.get_links())
+    print('links: ' + str(len(links)))
+    if len(links) > 50000: break
+for link in links:
+    bm = Bookmark(link)
+    words.extend(bm.get_words())
+    print('words: ' + str(len(words)))
+bm.get_bow(words)
 """
 class Hold All Spider Words
 what is it you're holding? words (tokens) from bookmark and
